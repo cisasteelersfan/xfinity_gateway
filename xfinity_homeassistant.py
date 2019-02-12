10a7028990f18d792300a370f16c69c687cca376
Colby Rome <cisasteelersfan@gmail.com>
2019-02-11 21:46:26 -0500

    proper logging

--- xfinity_homeassistant.py
+++ xfinity_homeassistant.py
@@ -1,21 +1,17 @@
 from src.xfinity_gateway.xfinity_gateway import XfinityGateway
 import logging
 
+from requests.exceptions import RequestException
 import voluptuous as vol
-
-import requests
-import json
 
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.device_tracker import (
     DOMAIN, PLATFORM_SCHEMA, DeviceScanner)
-from homeassistant.const import (
-    CONF_HOST, CONF_PASSWORD, CONF_USERNAME, CONF_PORT)
+from homeassistant.const import CONF_HOST
 
 _LOGGER = logging.getLogger(__name__)
 
 DEFAULT_HOST = '10.0.0.1'
-DEFAULT_PORT = 80
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_HOST, default=DEFAULT_HOST): cv.string
@@ -23,6 +19,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 
 def get_scanner(hass, config):
+    """Validate the configuration and return an Xfinity Gateway scanner."""
     info = config[DOMAIN]
     host = info.get(CONF_HOST)
 
@@ -30,22 +27,33 @@ def get_scanner(hass, config):
 
     return scanner if scanner.success_init else None
 
+
 class XfinityDeviceScanner(DeviceScanner):
+    """This class queries an Xfinity Gateway."""
 
     def __init__(self, address):
+        """Initialize the scanner."""
         try:
-            self.xfinity_gateway = XfinityGateway(address)
-            self.xfinity_gateway.scan_devices()
+            _LOGGER.debug('Initializing')
+            self.gateway = XfinityGateway(address)
+            self.gateway.scan_devices()
             self.success_init = True
-        except:
-            print("!!!!!! Error setting up gateway")
+        except (RequestException, ValueError):
             self.success_init = False
+            _LOGGER.error("Unable to connect to gateway. Check host: " + self.gateway.host)
 
     def scan_devices(self):
-        return self.xfinity_gateway.scan_devices()
+        """Scan for new devices and return a list of found MACs."""
+        connected_devices = []
+        try:
+            connected_devices = self.gateway.scan_devices()
+        except (RequestException, ValueError):
+            _LOGGER.error("Unable to scan devices. Check connection to gateway")
+        return connected_devices
 
     def get_device_name(self, device):
-        return self.xfinity_gateway.get_device_name(device)
+        """Return the name of the given device or None if we don't know."""
+        return self.gateway.get_device_name(device)
 
 
 
